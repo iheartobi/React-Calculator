fc2e958bc90269867d80e0fce5c972af810f4f36
iheartobi <iobidah9@gmail.com>
2020-03-26 16:23:57 -0400

    simple react calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      calcScreen: '' 
+    }
+  }
+
+handleKeyPress = (e) => {
+e.preventDefault()
+console.log(e.target)
+  }
+
+
+handleClick = e => {
+  e.preventDefault()
+  const value  = e.target.value
+  switch(value){
+    case '=': {
+      try{
+        const result = eval(this.state.calcScreen)
+        this.setState({calcScreen: result})
+      
+      }catch(error){
+        console.log(error)
+        this.setState({calsScreen: "Error"})
+
+      }
+      break
+    }
+    case 'all-clear': {
+      this.setState({calcScreen: ''})
+      break
+    }
+    default: {
+      this.setState({calscScreen: this.state.calcScreen += value})
+    }
+  }
+}
+
+  render() {
+    const { calcScreen } = this.state;
+    return (
+      <div className="app">
+        <h2 className="title">React Calculator</h2>
+        <div className="calculator">
+          <input type="text" className="calculator-screen" value={calcScreen} disabled />
+
+          <div onClick={this.handleClick} onKeyPress={this.handleKeyPress} className="calculator-keys">
+              <button type="button" className="operator" value='+'>
+                +
+              </button>
+              <button type="button" className="operator" value='-'>
+                -
+              </button>
+              <button type="button" className="operator" value='*'>
+                &times;
+              </button>
+              <button type="button" className="operator" value='/'>
+                &divide;
+              </button>
+
+              <button  type="button" value="7">
+                7
+              </button>
+              <button type="button" value="8">
+                8
+              </button>
+              <button type="button" value="9">
+                9
+              </button>
+
+              <button type="button" value="4">
+                4
+              </button>
+              <button type="button" value="5">
+                5
+              </button>
+              <button type="button" value="6">
+                6
+              </button>
+
+              <button type="button" value="1">
+                1
+              </button>
+              <button type="button" value="2">
+                2
+              </button>
+              <button type="button" value="3">
+                3
+              </button>
+
+              <button type="button" value="0">
+                0
+              </button>
+              <button type="button" className="decimal" value=".">
+                .
+              </button>
+              <button type="button" className="all-clear" value="all-clear">
+                AC
+              </button>
+
+              <button type="button" className="equal-sign" value="=">
+                =
+              </button>
+            </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
